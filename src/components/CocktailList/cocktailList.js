770817a498984199926a8ceb02024c01d07e6543
Emily Schroeder <estanfill0212@gmail.com>
2022-06-20 19:41:22 -0500

    oops

---
+++ src/components/CocktailList/cocktailList.js
@@ -0,0 +1,87 @@
+import React, { useContext } from "react"
+import cocktails from "../../assets/cocktails.json"
+import { useIngredientList } from "../IngredientsContext"
+import { useShoppingList } from "../ShoppingListContext"
+
+export const CocktailList = () => {
+    const [ingredientList, setIngredientList] = useIngredientList()
+    const [shoppingList, setShoppingList] = useShoppingList()
+    const addToShoppingList = (ingredient) => {
+        const newList = [...shoppingList, ingredient];
+
+        setShoppingList(newList);
+        console.log(newList);
+    }
+    const addToInventory = (ingredient) => {
+        const newList = [...ingredientList, ingredient];
+
+        setIngredientList(newList);
+        console.log(newList);
+    }
+    return (
+
+        <div className="container cocktails-list">
+            <div className="row">
+                {cocktails.Drinks.map(function (cocktail) {
+                   
+                 return (
+                <>
+                <div key={cocktail}>
+
+                {cocktailImageDisplay(cocktail.Name)}
+                <div>
+                Name: {cocktail.Name}
+                </div>
+                                                             
+                <div>
+                {cocktail.Ingredients.map ((ingredient) => {
+                return ( 
+                 <>
+                <div key={ingredient}>
+                <button onClick = {()=> addToInventory(ingredient.Ingredient)}>+ inventory</button>
+                Ingredients:{ingredient.Ingredient}
+                Measurement: {ingredient.Measurement}
+                <button onClick = {()=> addToShoppingList(ingredient.Ingredient)}>+ shopping list</button>
+                </div>
+                </>
+                )
+                                        
+                })
+                                    
+                }
+                                       
+                </div>                  
+                <div>
+                Method: {cocktail.Method}
+                </div>
+                <div>
+                Garnish: {cocktail.Garnish}
+                </div>
+                </div>
+                </>
+                )
+
+
+
+
+                }
+                )
+                }
+            </div>
+        </div>
+    )
+
+}
+
+export const cocktailImageDisplay = (Name) => {
+    return(
+        <div className="cocktail-img">
+            <img 
+            className="contain-fit"
+            src={`/cocktailImages/${Name}.jpg`} 
+            width="300"
+            alt = {`${Name}`}
+            />
+        </div>
+    )
+}
---
+++ src/components/IngredientsContext.js
@@ -0,0 +1,13 @@
+import { useContext, useState, createContext } from "react";
+
+const IngredientsListContext = createContext([]);
+export const useIngredientList = () => {
+    return useContext(IngredientsListContext)
+}
+
+export const IngredientListProvider = ({list, children}) => {
+    const [ingredientList, setIngredientList] = useState(list)
+return (<IngredientsListContext.Provider value={[ingredientList, setIngredientList]}>
+{children}
+    </IngredientsListContext.Provider>)
+}
