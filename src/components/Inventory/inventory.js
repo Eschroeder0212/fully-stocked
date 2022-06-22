bcadf12a3735ce267901a5a6006ede9f712289c5
Emily Schroeder <estanfill0212@gmail.com>
2022-06-21 20:20:13 -0500

    new file for delete

--- src/components/Inventory/inventory.js
+++ src/components/Inventory/inventory.js
@@ -8,6 +8,7 @@ import { useIngredientList } from '../IngredientsContext';
 
 
 export const MyInventory = () => {
+    const [ingredientList , ] = useIngredientList()
     return (
         <h1>My Bar</h1>
        
@@ -15,21 +16,3 @@ export const MyInventory = () => {
     )
 }
 
-export function DeleteFromInventory() {
-    const [ingredientList, setIngredientList] = React.useState(ingredientList);
-  
-    const deleteItem = (index) => () =>
-      setIngredientList((ingredientList) => ingredientList.filter((_, i) => i !== index));
-  
-    return (
-      <>
-        {ingredientList.map((it, index) => {
-          return (
-            <div key={it.name}>
-              {it.name} <button onClick={deleteItem(index)}>delete</button>
-            </div>
-          );
-        })}
-      </>
-    );
-  }
